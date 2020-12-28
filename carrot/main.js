e0d54963517356deb77076277459c7b760e8ea50
Ahyeong <aylee618@gmail.com>
2020-12-28 17:54:01 +0900

    주석, 음악 추가

--- carrot/main.js
+++ carrot/main.js
@@ -1,7 +1,15 @@
-const topMax = 20;
-const topMin = 12;
-const leftMax = 36;
-const leftMin = 1;
+/**
+ * top과 left를 아래처럼 조정하려고 했으나 아이템(벌레, 당근)들의 간격이 좁음
+ *  top: 240 ~ 430px
+ *  left: 10 ~ 730px
+ * 
+ * 그래서 20px씩 차이가 나게 하려고 아래와 같이 min, max 값을 정의함
+ */
+const TOP_MAX = 20;
+const TOP_MIN = 12;
+const LEFT_MAX = 36;
+const LEFT_MIN = 1;
+
 let timer=10;
 let count=10;
 let gameStart = false;
@@ -9,32 +17,59 @@ let gameStart = false;
 const game__timer = document.querySelector('.game__timer');
 const game__bugs = document.querySelector('.game__bugs');
 const game__carrots = document.querySelector('.game__carrots');
-const resultDialog = document.querySelector('dialog');
 const game__startBtn = document.querySelector('.game__startBtn');
 const game__stopBtn = document.querySelector('.game__stopBtn');
 const game__replayBtn = document.querySelector('.game__replayBtn');
 const game__count = document.querySelector('.game__count');
 const game__result = document.querySelector('.game__result');
 
+const resultDialog = document.querySelector('dialog');
+
+const sound__bg = document.querySelector('.sound__bg');
+const sound__alert = document.querySelector('.sound__alert');
+const sound__bug_pull = document.querySelector('.sound__bug_pull');
+const sound__carrot_pull = document.querySelector('.sound__carrot_pull');
+const sound__game_win = document.querySelector('.sound__game_win');
+
+/**
+ * 아이템을 배치할 랜덤한 top, left를 반환
+ * top: 240 ~ 400
+ * left: 20 ~ 720
+ * @return {object} top, left를 프로퍼티로 갖는 object를 반환
+ */
 function getRandomPosition() {
     return {
-        top: 20*Math.floor(Math.random() * (Math.floor(topMax) - Math.ceil(topMin)) + Math.ceil(topMin)),
-        left: 20*Math.floor(Math.random() * (Math.floor(leftMax) - Math.ceil(leftMin)) + Math.ceil(leftMin)),
+        top: 20 * Math.floor(Math.random() * (Math.floor(TOP_MAX) - Math.ceil(TOP_MIN)) + Math.ceil(TOP_MIN)),
+        left: 20 * Math.floor(Math.random() * (Math.floor(LEFT_MAX) - Math.ceil(LEFT_MIN)) + Math.ceil(LEFT_MIN)),
     }
 }
 
+/**
+ * 게임 종료시 호출됨
+ * resultDialog를 띄워주고 결과 메시지를 보여줌
+ * 
+ * @param {string} result 'win' 또는 'lost'
+ */
 function endGame(result) {
+    sound__bg.pause();
     gameStart=false;
     resultDialog.open = true;
     game__startBtn.style.display = 'none';
     game__stopBtn.style.display = 'none';
     if (result === 'lost') {
+        sound__alert.play();
         game__result.textContent = 'YOU LOST 😆';
     } else {
+        sound__game_win.play();
         game__result.textContent = 'YOU WIN 😉';
     }
 }
 
+/**
+ * 게임 타이머 - 1초씩 카운트다운
+ * 게임이 끝나면 멈추고
+ * timer가 0이 되면 endGame('lost') 호출
+ */
 function startTimer() {
     let timerId = setTimeout(function oneSecond() {
         if (!gameStart) {
@@ -44,12 +79,20 @@ function startTimer() {
         if(timer === 0){
             endGame('lost');
             return;
+        } else if (timer < 4) {
+            game__timer.style.color = 'red';
+            console.log(timer);
         }
         timerId = setTimeout(oneSecond, 1000);
     }, 1000);
 
 }
 
+/**
+ * 아이템(벌레, 당근)의 이미지 태그를 생성하여 리턴
+ * @param {string} imgSrc 'carrot' 또는 'bug'
+ * @return {HTMLImageElement} item 
+ */
 function getItemImageTag(imgSrc) {
     let zIndex;
     if (imgSrc === 'carrot') {
@@ -60,11 +103,13 @@ function getItemImageTag(imgSrc) {
         return;
     }
 
+    let item;
+
     const position = getRandomPosition();
-    bug = document.createElement('img');
-    bug.setAttribute('src', `./img/${imgSrc}.png`);
-    bug.setAttribute('alt', `${imgSrc}`);
-    bug.setAttribute(
+    item = document.createElement('img');
+    item.setAttribute('src', `./img/${imgSrc}.png`);
+    item.setAttribute('alt', `${imgSrc}`);
+    item.setAttribute(
         'style',
         `position: absolute; 
             top:${position.top}px; 
@@ -72,8 +117,13 @@ function getItemImageTag(imgSrc) {
             z-index:${zIndex};`
     );
 
-    return bug;
+    return item;
 }
+
+/**
+ * 게임판 세팅
+ * 벌레와 당근 10개씩 랜덤한 위치에 추가
+ */
 function setBoards() {
     let bug;
     let carrot;
@@ -86,18 +136,35 @@ function setBoards() {
     }
 }
 
+/**
+ * 게임 시작
+ * 게임판 세팅 : setBoards()
+ * 타이머 시작 : startTimer()
+ */
 function startGame()  {
     game__startBtn.style.display = 'none';
     game__stopBtn.style.display = 'inline';
+    sound__bg.play();
     setBoards();
     startTimer();
 }
 
-// startGame();
+/**
+ * 클릭 당한 당근 삭제
+ * @param {HTMLImageElement} target 
+ */
+function deleteCarrot(target) {
+    game__count.textContent =`${--count}`;
+    target.parentNode.removeChild(target);
+    if (count === 0) {
+        endGame('win');
+    }
+}
 
 game__startBtn.addEventListener('click', () => {
     gameStart=true;
     startGame();
+    sound__bg.play();
 });
 
 game__stopBtn.addEventListener('click', () => {
@@ -107,6 +174,7 @@ game__stopBtn.addEventListener('click', () => {
 game__replayBtn.addEventListener('click', () => {
     timer = 10;
     count = 10;
+    game__timer.style.color = 'black';
     game__startBtn.style.display = 'inline';
     game__timer.textContent = `0:${timer}`;
     game__count.textContent = `${count}`;
@@ -116,20 +184,13 @@ game__replayBtn.addEventListener('click', () => {
     
 });
 
-function deleteItem(target) {
-    game__count.textContent =`${--count}`;
-    target.parentNode.removeChild(target);
-    if (count === 0) {
-        endGame('win');
-    }
-}
-
 game__carrots.addEventListener('click', (e) => {
     if (!gameStart){
         return;
     }
     if (e.target.tagName === 'IMG') {
-        deleteItem(e.target);
+        sound__carrot_pull.play();
+        deleteCarrot(e.target);
     }
 });
 
@@ -138,6 +199,7 @@ game__bugs.addEventListener('click', (e) => {
         return;
     }
     if (e.target.tagName === 'IMG') {
+        sound__bug_pull.play();
         endGame('lost');
     }
 });
